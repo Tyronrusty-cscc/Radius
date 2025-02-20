040cb1e3e6026105a77a9fe57ef8800756bad931
trusty <trusty@cscc.edu>
2025-02-20 13:23:02 -0500

    changes the paths to it default

--- app/Config/Paths.php
+++ app/Config/Paths.php
@@ -29,7 +29,7 @@ class Paths
      * the path if the folder is not in the same directory as this file.
      */
     #public string $systemDirectory = __DIR__ . '/../../system';
-	public $systemDirectory;
+	public $systemDirectory = 'C:\data\Codeigniter-4.6.0-Shared\system';
 
     /**
      * ---------------------------------------------------------------
@@ -79,16 +79,5 @@ class Paths
      */
     public string $viewDirectory = __DIR__ . '/../Views';
 
-    public function __construct()
-    {
-        // Check if running in GitHub Actions
-        if (getenv('GITHUB_ACTIONS') === 'true') {
-            $this->systemDirectory = realpath(__DIR__ . '/../../vendor/codeigniter4/framework/system')
-                ?: 'vendor/codeigniter4/framework/system';
-        } else {
-            // Default system path for local development
-            $this->systemDirectory = realpath(__DIR__ . '/../../system')
-                ?: 'C:\data\Codeigniter-4.6.0-Shared\system';
-        }
-    }
+    
 }
